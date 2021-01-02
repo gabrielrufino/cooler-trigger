1ffe2f1b23524397e5e95a69d1ee8e38071ac2ec
Gabriel Rufino <contato@gabrielrufino.com>
2021-01-02 16:55:10 -0300

    Initial algorithm

---
+++ index.js
@@ -0,0 +1,28 @@
+const pWaitFor = require('p-wait-for')
+const shell = require('shelljs')
+const si = require('systeminformation')
+
+const deactivationThreshold = 65
+const activationThreshold = 45
+
+async function main() {
+  await pWaitFor(async () => {
+    const { main } = await si.cpuTemperature()
+
+    return main > deactivationThreshold
+  })
+
+  shell.exec('sudo uhubctl -l 1-1 -p 2 -a 1')
+
+  await pWaitFor(async () => {
+    const { main } = await si.cpuTemperature()
+
+    return main < activationThreshold
+  })
+
+  shell.exec('sudo uhubctl -l 1-1 -p 2 -a 0')
+
+  main()
+}
+
+main()
